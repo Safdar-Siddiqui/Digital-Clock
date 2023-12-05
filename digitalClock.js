6ae79b60bcdb0f6145a11dea3136a2c02884d644
Safdar-Siddiqui <safdarsiddiqui25@gmail.com>
2023-12-06 00:58:04 +0500

    Digital Clock

---
+++ digitalClock.js
@@ -0,0 +1,30 @@
+function clock(){
+let date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+
+let amPM = "AM";
+if(hours >= 12){
+  hours = hours-12;
+  amPM = "PM"
+}
+if(hours <= 9){
+  hours = "0" + hours;
+}
+if(seconds <= 9){
+  seconds = "0" + seconds;
+}
+if(minutes <= 9){
+  minutes = "0" + minutes;
+}
+
+document.getElementById('hrs').innerHTML = hours;
+document.getElementById('mins').innerHTML = minutes;
+document.getElementById('secs').innerHTML = seconds;
+document.getElementById('am-pm').innerHTML = amPM;
+
+
+}
+setInterval(clock, 1000);
+
