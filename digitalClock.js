6c71485071d955105dd4e9ada29c35af4019d0b8
Safdar-Siddiqui <safdarsiddiqui25@gmail.com>
2023-12-15 23:06:52 +0500

    modified

--- digitalClock.js
+++ digitalClock.js
@@ -1,30 +1,36 @@
-function clock(){
-let date = new Date();
-let hours = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
+function clock() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
 
-let amPM = "AM";
-if(hours >= 12){
-  hours = hours-12;
-  amPM = "PM"
-}
-if(hours <= 9){
-  hours = "0" + hours;
-}
-if(seconds <= 9){
-  seconds = "0" + seconds;
-}
-if(minutes <= 9){
-  minutes = "0" + minutes;
+  let amPM = "AM";
+  if (hours >= 12) {
+    hours = hours - 12;
+    amPM = "PM";
+  }
+  if (hours == 0) {
+    hours = 12;
+    amPM = "AM";
+  }
+
+  if (hours <= 9) {
+    hours = "0" + hours;
+  }
+
+  if (seconds <= 9) {
+    seconds = "0" + seconds;
+  }
+
+  if (minutes <= 9) {
+    minutes = "0" + minutes;
+  }
+
+  document.getElementById("hrs").innerHTML = hours;
+  document.getElementById("mins").innerHTML = minutes;
+  document.getElementById("secs").innerHTML = seconds;
+  document.getElementById("am-pm").innerHTML = amPM;
+  document.getElementById("date").innerHTML = date.toDateString();
 }
 
-document.getElementById('hrs').innerHTML = hours;
-document.getElementById('mins').innerHTML = minutes;
-document.getElementById('secs').innerHTML = seconds;
-document.getElementById('am-pm').innerHTML = amPM;
-
-
-}
 setInterval(clock, 1000);
-
