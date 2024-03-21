ab98693a324d0cb440c79ca887ad630759bc63ed
Safdar-Siddiqui <safdarsiddiqui25@gmail.com>
2024-03-22 04:48:17 +0500

    modified

--- digitalClock.js
+++ digitalClock.js
@@ -13,69 +13,48 @@ function clock() {
   var seconds = date.getSeconds();
 
   var secondsDots = '';
-  for (var i =1; i<61; i++){
-    var rotation = i*6;
-    if(i === seconds){
-      secondsDots += '<div class="dot active" style="transform: rotate('+rotation+'deg)"> </div>'
+  for (var i = 1; i < 61; i++) {
+    var rotation = i * 6;
+    if (i === seconds) {
+      secondsDots += '<div class="dot active" style="transform: rotate(' + rotation + 'deg)"> </div>'
     }
-    else{
-      secondsDots += '<div class="dot" style="transform: rotate('+rotation+'deg)"> </div>'
+    else {
+      secondsDots += '<div class="dot" style="transform: rotate(' + rotation + 'deg)"> </div>'
     }
   }
 
   var minutesDots = '';
-  for (var i =1; i<61; i++){
-    var rotation = i*6;
-    if(i === minutes){
-      minutesDots += '<div class="dot active" style="transform: rotate('+rotation+'deg)"> </div>'
+  for (var i = 1; i < 61; i++) {
+    var rotation = i * 6;
+    if (i === minutes) {
+      minutesDots += '<div class="dot active" style="transform: rotate(' + rotation + 'deg)"> </div>'
     }
-    else{
-      minutesDots += '<div class="dot" style="transform: rotate('+rotation+'deg)"> </div>'
+    else {
+      minutesDots += '<div class="dot" style="transform: rotate(' + rotation + 'deg)"> </div>'
     }
   }
 
   var hoursDots = '';
-  for (var i =1; i<13; i++){
-    var rotation = i*30;
-    if(i === hours){
-      hoursDots += '<div class="dot active" style="transform: rotate('+rotation+'deg)"> </div>'
+  for (var i = 1; i < 13; i++) {
+    var rotation = i * 30;
+    if (i === hours) {
+      hoursDots += '<div class="dot active" style="transform: rotate(' + rotation + 'deg)"> </div>'
     }
-    else{
-      hoursDots += '<div class="dot" style="transform: rotate('+rotation+'deg)"> </div>'
+    else {
+      hoursDots += '<div class="dot" style="transform: rotate(' + rotation + 'deg)"> </div>'
     }
   }
-
 
   secDots.innerHTML = secondsDots + '<b>' + amPm + '</b>' + '<h2>' + zero(seconds) + '<br> <span> Seconds </span> </h2>';
   minDots.innerHTML = minutesDots + '<h2>' + zero(minutes) + '<br> <span> Minutes </span> </h2>';
   hrDots.innerHTML = hoursDots + '<h2>' + zero(hours) + '<br> <span> Hours </span> </h2>';
 }
 
-
-  // let amPM = "AM";
-  // if (hours >= 12) {
-  //   hours = hours - 12;
-  //   amPM = "PM";
-  // }
-  // if (hours == 0) {
-  //   hours = 12;
-  //   amPM = "AM";
-  // }
-
-  function zero(number){
-    if(number <10){
-      return '0' + number; 
-    }
-    return number;
+function zero(number) {
+  if (number < 10) {
+    return '0' + number;
   }
-
-
-
-  // document.getElementById("hrs").innerHTML = hours;
-  // document.getElementById("mins").innerHTML = minutes;
-  // document.getElementById("secs").innerHTML = seconds;
-  // document.getElementById("am-pm").innerHTML = amPM;
-  // document.getElementById("date").innerHTML = date.toDateString();
-
+  return number;
+}
 
 setInterval(clock, 1000);
